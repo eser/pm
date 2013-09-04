272cf85f1fb821950428fed1ac99165542d0207d
larukedi <eser@sent.com>
2013-09-04 15:38:50 +0300

    * role crud.

--- htdocs/application/controllers/manage.php
+++ htdocs/application/controllers/manage.php
@@ -3,7 +3,9 @@
 namespace App\Controllers;
 
 use Scabbia\Extensions\Mvc\Controller;
+use Scabbia\Extensions\Http\Http;
 use Scabbia\Extensions\Fb\Fb;
+use Scabbia\Request;
 
 /**
  * @ignore
@@ -15,6 +17,8 @@ class manage extends Controller
      */
     public function index()
     {
+        // Auth::checkRedirect('user');
+
         $this->load('App\\Models\\homeModel');
 
         $tWelcomeText = $this->homeModel->getWelcomeText();
@@ -26,40 +30,154 @@ class manage extends Controller
     /**
      * @ignore
      */
-    public function users()
+    public function users($uSubpage = 'index')
+    {
+        // Auth::checkRedirect('user');
+
+        if ($uSubpage === 'index') {
+            return $this->users_index();
+        } elseif ($uSubpage === 'add') {
+            return $this->users_add();
+        } elseif ($uSubpage === 'edit') {
+            return $this->users_edit();
+        } elseif ($uSubpage === 'remove') {
+            return $this->users_remove();
+        }
+
+        return false;
+    }
+
+    /**
+     * @ignore
+     */
+    public function groups($uSubpage = 'index')
+    {
+        // Auth::checkRedirect('user');
+
+        if ($uSubpage === 'index') {
+            return $this->groups_index();
+        } elseif ($uSubpage === 'add') {
+            return $this->groups_add();
+        } elseif ($uSubpage === 'edit') {
+            return $this->groups_edit();
+        } elseif ($uSubpage === 'remove') {
+            return $this->groups_remove();
+        }
+
+        return false;
+    }
+
+
+    /**
+     * @ignore
+     */
+    public function roles($uSubpage = 'index', $id = 0)
+    {
+        // Auth::checkRedirect('user');
+
+        if ($uSubpage === 'index') {
+            return $this->roles_index();
+        } elseif ($uSubpage === 'add') {
+            return $this->roles_add();
+        } elseif ($uSubpage === 'edit') {
+            return $this->roles_edit($id);
+        } elseif ($uSubpage === 'remove') {
+            return $this->roles_remove($id);
+        }
+
+        return false;
+    }
+
+    /**
+     * @ignore
+     */
+    private function users_index()
     {
         $this->load('App\\Models\\userModel');
 
         $tUsers = $this->userModel->getUsers();
         $this->set('users', $tUsers);
 
-        $this->view();
+        $this->view('manage/users/index.cshtml');
     }
 
     /**
      * @ignore
      */
-    public function groups()
+    private function groups_index()
     {
         $this->load('App\\Models\\groupModel');
 
         $tGroups = $this->groupModel->getGroups();
         $this->set('groups', $tGroups);
 
-        $this->view();
+        $this->view('manage/groups/index.cshtml');
     }
-
 
     /**
      * @ignore
      */
-    public function roles()
+    private function roles_index()
     {
         $this->load('App\\Models\\roleModel');
 
         $tRoles = $this->roleModel->getRoles();
         $this->set('roles', $tRoles);
 
-        $this->view();
+        $this->view('manage/roles/index.cshtml');
+    }
+
+    /**
+     * @ignore
+     */
+    private function roles_add()
+    {
+        if (Request::$method === 'post') {
+            $this->load('App\\Models\\roleModel');
+
+            $tId = $this->roleModel->insert(
+                array(
+                    'name' => Request::post('name', null, null),
+                    'createproject' => Request::post('createproject', null, null),
+                    'createuser' => Request::post('createuser', null, null),
+                    'deleteuser' => Request::post('deleteuser', null, null)
+                )
+            );
+
+            // redirect to newly created
+            Http::redirect('manage/roles/edit/' . $tId, true);
+            return;
+        }
+
+        $this->view('manage/roles/add.cshtml');
+    }
+
+    /**
+     * @ignore
+     */
+    private function roles_edit($uId)
+    {
+        $this->load('App\\Models\\roleModel');
+
+        if (Request::$method === 'post') {
+            $tData = array(
+                'name' => Request::post('name', null, null),
+                'createproject' => Request::post('createproject', null, null),
+                'createuser' => Request::post('createuser', null, null),
+                'deleteuser' => Request::post('deleteuser', null, null)
+            );
+
+            $this->roleModel->update(
+                $uId,
+                $tData
+            );
+        } else {
+            $tData = $this->roleModel->get($uId);
+        }
+
+        $this->set('id', $uId);
+        $this->set('data', $tData);
+
+        $this->view('manage/roles/edit.cshtml');
     }
 }
--- htdocs/application/models/roleModel.php
+++ htdocs/application/models/roleModel.php
@@ -20,4 +20,84 @@ class roleModel extends Model
             ->get()
             ->all();
     }
+
+    /**
+     * @ignore
+     */
+    public function getRolesWithPaging($uOffset = 0, $uLimit = 20)
+    {
+        $tResult = $this->db->createQuery()
+            ->setTable('siteroles')
+            ->setFields('*')
+            ->setOffset($uOffset)
+            ->setLimit($uLimit)
+            ->get()
+            ->all();
+
+        return $tResult;
+    }
+
+    /**
+     * @ignore
+     */
+    public function get($uId)
+    {
+        $tResult = $this->db->createQuery()
+            ->setTable('siteroles')
+            ->setFields('*')
+            ->setWhere('id=:id')
+            ->setLimit(1)
+            ->addParameter('id', $uId)
+            ->get()
+            ->row();
+
+        return $tResult;
+    }
+
+    /**
+     * @ignore
+     */
+    public function insert($insert)
+    {
+        $tResult = $this->db->createQuery()
+            ->setTable('siteroles')
+            ->setFields($insert)
+            ->insert()
+            ->execute(true);
+
+        return $tResult;
+    }
+
+    /**
+     * @ignore
+     */
+    public function update($id, $update)
+    {
+        $tResult = $this->db->createQuery()
+            ->setTable('siteroles')
+            ->setFields($update)
+            ->addParameter('id', $id)
+            ->setWhere('id=:id')
+            ->setLimit(1)
+            ->update()
+            ->execute();
+
+        return $tResult;
+    }
+
+    /**
+     * @ignore
+     */
+    public function delete($uId)
+    {
+        $tResult = $this->db->createQuery()
+            ->setTable('siteroles')
+            ->setWhere('id=:id')
+            ->addParameter('id', $uId)
+            ->setLimit(1)
+            ->delete()
+            ->execute();
+
+        return $tResult;
+    }
 }
