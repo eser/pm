8689a5d33a09223bec1bc59ac8f6e25cdea133bc
larukedi <eser@sent.com>
2014-02-09 05:38:34 +0200

    * database updates.

--- db/R4.sql
+++ db/R4.sql
@@ -42,3 +42,17 @@ UPDATE `roles` SET `login`='1', `active`='1';
 
 ALTER TABLE `users`   
   CHANGE `siterole` `role` INT(10) UNSIGNED NOT NULL;
+
+ALTER TABLE `tasks`   
+  ADD COLUMN `duedate` DATETIME NULL AFTER `enddate`;
+
+CREATE TABLE `project_users`(
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `project` INT UNSIGNED NOT NULL,
+  `user` INT UNSIGNED NOT NULL,
+  `relation` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+
+ALTER TABLE `constants`   
+  CHANGE `type` `type` ENUM('task_type','project_type','open_task_type','closed_task_type','priority_type','project_relation_type') CHARSET utf8 COLLATE utf8_general_ci NOT NULL;
